408b8da5b3ef47cbb3341460a23dbefec2490fe6
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-09-19 16:39:46 +0500

    Added methods user input and data processing/display

--- src/index.js
+++ src/index.js
@@ -1,40 +1,93 @@
 import "./styles.css";
 
+const searchForm = document.querySelector("#search-form");
+const displayDiv = document.querySelector(".info-container");
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const textInput = searchForm.querySelector("#location-input");
+    if (!textInput || textInput.value === "") return;
+    fetchWeather(textInput.value);
+    searchForm.reset();
+});
+
 async function getWeatherData(location = "China") {
     const apiKey = "299c515348484e4db1795648251909";
-    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location.trim()}`;
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
 
     try {
         const response = await fetch(url);
 
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            showError();
         }
 
         const weatherData = await response.json();
         console.log("Weather data: ", weatherData);
-        return weatherData;
+
+        const formattedData = processData(weatherData);
+        renderData(formattedData);
 
     } catch (error) {
-        console.error("Failed to get weather data: ", error);
-        return null;
+        console.error("Failed to get weather data");
     }
 }
 
-function throwError() {
-    // display error
+function showError() {
+    displayDiv.textContent = "Location not found";
 }
 
 function processData(data) {
-    // process data
+    const myData = {
+        condition: {
+            text: data.current.condition.text,
+            icon: data.current.condition.icon
+        },
+        currentTemp: {
+            metric: Math.floor(data.current.temp_c),
+            imperial: Math.floor(data.current.temp_f),
+        },
+        feelsLike: {
+            metric: Math.floor(data.current.feelslike_c),
+            imperial: Math.floor(data.current.feelslike_f),
+        },
+        windSpeed: {
+            metric: Math.floor(data.current.wind_kph),
+            imperial: Math.floor(data.current.wind_mph),
+        },
+        humidity: data.current.humidity,
+        region: data.location.region,
+        country: data.location.country
+    }
+
+    return myData;
 }
 
-function displayData(data) {
-    // Handle display
+function renderData(data) {
+    // Metric
+    displayDiv.innerHTML = `
+    <img src=${data.condition.icon}><br>
+    Condition: ${data.condition.text}<br>
+    Temperature: ${data.currentTemp.metric}&deg;C<br>
+    Feels Like: ${data.feelsLike.metric}&deg;C<br>
+    Wind: ${data.windSpeed.metric} km/h<br>
+    Humidity: ${data.humidity}%<br>
+    Location: ${data.region}, ${data.country}<br>
+    `;
+
+    // Imperial
+    displayDiv.innerHTML = `
+    <img src=${data.condition.icon}><br>
+    Condition: ${data.condition.text}<br>
+    Temperature: ${data.currentTemp.imperial}&deg;F<br>
+    Feels Like: ${data.feelsLike.imperial}&deg;F<br>
+    Wind: ${data.windSpeed.imperial} mp/h<br>
+    Humidity: ${data.humidity}%<br>
+    Location: ${data.region}, ${data.country}<br>
+    `;
 }
 
-function fetchWeather() {
-    // Handle user input
+function fetchWeather(location) {
+    const input = location.trim();
+    getWeatherData(input);
 }
-
-getWeatherData();
